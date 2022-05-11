8447dc2e0480f965b4ebc2daae652aa852c8567b
Guilherme Felix <1817960@aluno.univesp.br>
2022-05-11 19:50:37 -0300

    Configurando o TypeScript e Tambem o CSS em cascata SASS

---
+++ landingpage/src/pages/Newuser.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Newuser.module.scss';
+
+export default function Newuser() {
+    return (
+      <div>
+        <h1 className={styles.title}>Novo Usuario</h1>
+      </div>
+    )
+  }
+  
