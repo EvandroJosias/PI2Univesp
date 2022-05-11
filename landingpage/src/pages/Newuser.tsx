fb16980dbc577f88e11ad5a6f9dbacb279a247fc
Guilherme Felix <1817960@aluno.univesp.br>
2022-05-11 20:12:08 -0300

    Inserindo title na pagina

--- landingpage/src/pages/Newuser.tsx
+++ landingpage/src/pages/Newuser.tsx
@@ -1,10 +1,18 @@
 import styles from '../styles/Newuser.module.scss';
+import Head from 'next/head';
 
 export default function Newuser() {
     return (
-      <div>
-        <h1 className={styles.title}>Novo Usuario</h1>
-      </div>
+      <>
+        <Head>
+          <title>
+            Novo Usuario
+          </title>
+        </Head>
+        <div>
+          <h1 className={styles.title}>Novo Usuario</h1>
+        </div>
+      </>
     )
   }
   
