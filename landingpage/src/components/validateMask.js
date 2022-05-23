3e4023452f5d9c77b4965e26609ebb0cfe647489
ejsjose <ejsjose@kyndryl.com>
2022-05-23 20:21:40 -0300

    Update Mai23 Night

--- landingpage/src/components/validateMask.js
+++ landingpage/src/components/validateMask.js
@@ -1,20 +1,22 @@
 
 
 function mask_Phone( parPhone ){
-    const textoAtual = parPhone.value;
-    const isCelular = textoAtual.length === 9;
-    let textoAjustado;
-    if(isCelular) {
-         // faz alguma coisa 
-    } else {
-         // faz alguma coisa
-    }
-    parPhone.value = textoAjustado;
+    console.log('mask_Phone');
+    // const textoAtual = parPhone.value;
+    // const isCelular = textoAtual.length === 9;
+    // let textoAjustado;
+    // if(isCelular) {
+    //      // faz alguma coisa 
+    // } else {
+    //      // faz alguma coisa
+    // }
+    // parPhone.value = textoAjustado;
 }
 
 function cut_Hifen( parText ) {
-    const textoAtual = parText.value;
-    const textoAjustado = textoAtual.replace(/\-/g, '');
+    // const textoAtual = parText.value;
+    // const textoAjustado = textoAtual.replace(/\-/g, '');
 
-    parText.value = textoAjustado;
+    // parText.value = textoAjustado;
+    console.log('cut_Hifen');
 }
